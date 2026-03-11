66a21d51d634daad67866f2e2ce1d981660e9786
WeenorMan <520993@escg.ac.uk>
2026-03-11 12:02:32 +0000

    first publish

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private float walkSpeed = 3f;
+    [SerializeField] private float fallMultiplier = 2f;
+
+    private bool isGrounded;
+    private PlayerInputHandler inputHandler;
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        inputHandler = PlayerInputHandler.Instance;
+        isGrounded = true;
+    }
+
+    private void FixedUpdate()
+    {
+        if (!GetIsGrounded())
+        {
+            rb.linearVelocity += Vector2.up * Physics2D.gravity.y * fallMultiplier * Time.fixedDeltaTime;
+            //if (GetIsGrounded())
+            //{
+            //    isGrounded = true;
+            //    rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0);
+            //    Debug.Log(isGrounded);
+            //}
+        }
+       
+    }
+
+    void Update()
+    {
+        Debug.DrawRay(transform.position, Vector2.down * 0.75f, Color.red);
+
+        if (inputHandler.JumpTriggered && GetIsGrounded())
+        {
+            Debug.Log("jump pressed");
+            Jump();
+            isGrounded = false;
+        } 
+
+    }
+
+    private bool GetIsGrounded()
+    {
+        return Physics2D.Raycast(transform.position, Vector2.down, 0.75f, LayerMask.GetMask("Ground"));
+    }
+
+    private void Jump()
+    {
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+    } 
+
+    void CheckForLanding()
+    {
+        if(rb.linearVelocity.y < 0.1f)
+        {
+
+        }
+    }
+}
