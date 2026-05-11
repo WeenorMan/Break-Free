8060f4ea34cc10c8eaa97ea9ffed8cbf8fcf0a54
HarveyTicehurst <520993@escg.ac.uk>
2026-05-12 00:24:10 +0100

    Main Menu Assets
    added assets for the main menu and started using them to make my game look nicer, also added an audio manager

---
+++ Assets/Scripts/Backend Logic/AudioManager.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+    public static bool isGameEnding = false;
+
+    [SerializeField] AudioMixer mixer;
+
+    public AudioClip[] clips;
+    public AudioSource musicAudioSource;
+    public AudioSource sfxAudioSource;
+
+    public const string MASTER_KEY = "Volume";
+    public const string MUSIC_KEY = "musicVolume";
+    public const string SFX_KEY = "sfxVolume";
+
+
+    private void Awake()
+    {
+        isGameEnding = false;
+
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            print("do not destroy");
+        }
+        else
+        {
+            print("do destroy");
+            Destroy(gameObject);
+        }
+
+        LoadVolume();
+        //PlayMusicClip(1);
+    }
+
+    public void PlaySFXClip(int clipNumber)
+    {
+        sfxAudioSource.PlayOneShot(clips[clipNumber]); // start clip  
+    }
+    public void PlayMusicClip(int clipNumber)
+    {
+        musicAudioSource.PlayOneShot(clips[clipNumber]); // start clip  
+    }
+
+    public void StopSFXClip()
+    {
+        sfxAudioSource.Stop(); //stop currently playing clip  
+    }
+    public void StopMusicClip()
+    {
+        musicAudioSource.Stop(); //stop currently playing clip  
+    }
+
+    public void OnPlayerDeath()
+    {
+        isGameEnding = true;
+        Debug.Log("Player died");
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    
+    
+
+    void LoadVolume()
+    {
+        float masterVolume = PlayerPrefs.GetFloat(MASTER_KEY, 1f);
+        float musicVolume = PlayerPrefs.GetFloat(MUSIC_KEY, 1f);
+        float sfxVolume = PlayerPrefs.GetFloat(SFX_KEY, 1f);
+
+        mixer.SetFloat(VolumeSettings.MIXER_MASTER, Mathf.Log10(masterVolume) * 20);
+        mixer.SetFloat(VolumeSettings.MIXER_MUSIC, Mathf.Log10(musicVolume) * 20);
+        mixer.SetFloat(VolumeSettings.MIXER_SFX, Mathf.Log10(sfxVolume) * 20);
+    }
+}
